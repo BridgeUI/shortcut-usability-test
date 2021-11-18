3cfe273ecb93555f77cd813a80a130dfb3feee93
Yuji Sasaki <jyu0414@gmail.com>
2021-11-18 12:20:59 +0900

    set format

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,56 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'shortcut-usability-test',
+    title: "shortcut-usability-test",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt",
+    [
+      "@nuxtjs/firebase",
+      {
+        config: {
+          apiKey: "AIzaSyCUZ0YhK--1Kga7WKd3bQLYLDbQ8fZRH5E",
+          authDomain: "usability-test-3e101.firebaseapp.com",
+          projectId: "usability-test-3e101",
+          storageBucket: "usability-test-3e101.appspot.com",
+          messagingSenderId: "443121556273",
+          appId: "1:443121556273:web:ee7e9e65ed4fbf8083ee10",
+        },
+        services: {
+          firestore: true,
+        },
+      },
+    ],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,83 @@
 <template>
-  <Tutorial/>
+  <b-container class="mt-5">
+    <div v-if="!loading">
+      <div>
+        <b-button class="mb-5" variant="primary" @click="send">送信</b-button>
+      </div>
+      <b-form-group label="To:">
+        <b-form-input
+          type="email"
+          placeholder="宛先"
+          v-model="mail"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="件名:">
+        <b-form-input
+          type="text"
+          placeholder="件名"
+          v-model="title"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="本文:">
+        <b-form-textarea
+          id="textarea"
+          v-model="body"
+          placeholder="Enter something..."
+          rows="40"
+          max-rows="40"
+        ></b-form-textarea>
+      </b-form-group>
+      <b-form-group label="実験ID（触らないでください）:">
+        <b-form-input
+          type="text"
+          placeholder="ID"
+          v-model="id"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div v-if="loading" class=".mx-auto">
+      <b-spinner label="Loading..."></b-spinner>
+    </div>
+  </b-container>
 </template>
 
 <script>
-export default {}
+import {
+  getFirestore,
+  addDoc,
+  serverTimestamp,
+  collection,
+} from "firebase/firestore";
+
+export default {
+  data() {
+    return {
+      body: "",
+      title: "",
+      mail: "",
+      id: "",
+      loading: false
+    };
+  },
+  methods: {
+    async send() {
+      this.loading = true
+      const col = collection(getFirestore(), `activities/${this.id}/messages`);
+      const result = await addDoc(col, {
+        datetime: serverTimestamp(),
+        title: this.title,
+        mail: this.mail,
+        body: this.body,
+        id: this.id,
+      });
+      this.title = ""
+      this.mail = ""
+      this.body = ""
+      this.loading = false
+    },
+  },
+};
 </script>
